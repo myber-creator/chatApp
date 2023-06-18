07025435aaca0070de5c84065433d7205c2db8f2
myber-creator <jeffrimorgan@yandex.ru>
2023-06-18 19:45:58 +0700

    change typeorm config

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -5,11 +5,12 @@ export const getTypeOrmConfig = async (
   configService: ConfigService,
 ): Promise<TypeOrmModuleOptions> => ({
   type: 'postgres',
-  host: configService.get('HOST'),
-  port: configService.get('PORT'),
-  database: configService.get('DATABASE'),
-  username: configService.get('USER'),
-  password: configService.get('PASSWORD'),
+  host: configService.get('PGHOST'),
+  port: configService.get('PGPORT'),
+  database: configService.get('PGDATABASE'),
+  username: configService.get('PGUSER'),
+  password: configService.get('PGPASSWORD'),
   autoLoadEntities: true,
   synchronize: true,
+  url: configService.get('DATABASE_URL'),
 });
